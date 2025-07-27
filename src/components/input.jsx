cfe101fe24eab7e3feabb41a88e050db3b133c9d
1Alraza <alrazaquadri62@gmail.com>
2025-07-27 22:38:05 +0530

    Fix case-sensitive imports and build issues for Vercel

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,7 +1,7 @@
 'use server'
 
 import { getCurrentUser } from '@/lib/actions/user';
-import InputClient from './Inputclient';
+import InputClient from './InputClient';
 
 export default async function Input() {
   const user = await getCurrentUser(); 
